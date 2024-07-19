36dedc6a45b4a361c92deb1f80a7d7d06cc1a974
haeuni00 <leejw6311@naver.com>
2024-07-19 20:12:14 +0900

    🩹 fix: Timestamped, ExtendedTimestamped 컬럼명 수정
    - Timestamped 컬럼명 time -> date로 변경
    - ExtendedTimestamped 컬럼명 deleted -> delete 변경
    - ExtendedTimestamped @EntityListeners 삭제

--- src/main/java/com/sparta/goodbite/common/ExtendedTimestamped.java
+++ src/main/java/com/sparta/goodbite/common/ExtendedTimestamped.java
@@ -1,17 +1,14 @@
 package com.sparta.goodbite.common;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class ExtendedTimestamped extends Timestamped {
 
-    @Column(name = "deleted_date")
+    @Column(name = "delete_date")
     private LocalDateTime deletedAt;
 }

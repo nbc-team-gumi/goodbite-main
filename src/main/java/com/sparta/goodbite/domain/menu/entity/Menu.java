ecc90173130e238b998f446a7f37d96bfb044279
soyoung <soyoung.yang94@gmail.com>
2024-07-19 20:35:36 +0900

    🛠️ build: 빌드 수정
    - application.yml 파일 jpa 추가
    - build.gradle 파일 주석 추가

--- src/main/java/com/sparta/goodbite/domain/menu/entity/Menu.java
+++ src/main/java/com/sparta/goodbite/domain/menu/entity/Menu.java
@@ -2,8 +2,11 @@ package com.sparta.goodbite.domain.menu.entity;
 
 import com.sparta.goodbite.common.Timestamped;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Menu extends Timestamped {
 
+    @Id
+    private long id;
 }

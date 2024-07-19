421938e642fbad9d72b177ddb69675046a312ac1
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-19 20:12:28 +0900

    🩹 fix: 추상 클래스로 변경
    - ExtendedTimestamped는 인스턴스를 생성하지 않기 때문에 추상 클래스로 변경

--- src/main/java/com/sparta/goodbite/common/ExtendedTimestamped.java
+++ src/main/java/com/sparta/goodbite/common/ExtendedTimestamped.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class ExtendedTimestamped extends Timestamped {
+public abstract class ExtendedTimestamped extends Timestamped {
 
     @Column(name = "deleted_date")
     private LocalDateTime deletedAt;

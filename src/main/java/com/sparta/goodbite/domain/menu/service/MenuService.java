40e6a875b5a9fd219ebe1744dc827f4045f9ee7e
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-20 02:28:47 +0900

    ✨ feat: 메뉴 생성 메서드 추가 및 의존성 주입
    - 메뉴 생성하는 createMenu 메서드 임시 추가
    - menuRepository 의존성 주입

--- src/main/java/com/sparta/goodbite/domain/menu/service/MenuService.java
+++ src/main/java/com/sparta/goodbite/domain/menu/service/MenuService.java
@@ -1,8 +1,19 @@
 package com.sparta.goodbite.domain.menu.service;
 
+import com.sparta.goodbite.domain.menu.dto.CreateMenuRequestDto;
+import com.sparta.goodbite.domain.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class MenuService {
 
+    private final MenuRepository menuRepository;
+
+    @Transactional
+    public void createMenu(CreateMenuRequestDto createMenuRequestDto) { // User user
+        menuRepository.save(createMenuRequestDto.toEntity());
+    }
 }

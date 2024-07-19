a8c50f3a8b06fc847e54761c3dc7fa6e5107eefe
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-20 02:32:06 +0900

    ✨ feat: 메뉴 생성 엔드포인트 추가
    - MenuService의 메뉴 생성 메서드를 사용하여 메뉴 생성 엔드포인트 임시 구현

--- src/main/java/com/sparta/goodbite/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/goodbite/domain/menu/controller/MenuController.java
@@ -2,9 +2,13 @@ package com.sparta.goodbite.domain.menu.controller;
 
 import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
+import com.sparta.goodbite.domain.menu.dto.CreateMenuRequestDto;
 import com.sparta.goodbite.domain.menu.service.MenuService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +19,12 @@ public class MenuController {
 
     private final MenuService menuService;
 
-    public ResponseEntity<MessageResponseDto> createMenu() {
+    @PostMapping
+    public ResponseEntity<MessageResponseDto> createMenu(
+        @RequestBody @Valid CreateMenuRequestDto createMenuRequestDto) { // @AuthenticationPricipal UserDetailsImpl userDetails
+
+        menuService.createMenu(
+            createMenuRequestDto); // menuService.createMenu(createMenuRequestDto, user);
         return ResponseUtil.createOk();
     }
 }

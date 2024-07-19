8bd6a6e366e5536d67ac116946e01a6c59f8cefa
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-20 02:27:06 +0900

    ✨ feat: Menu 엔티티 필드 및 생성자 빌더 패턴 추가
    - id, price, name, description 필드 추가
    - @Builder 애너테이션을 사용하여 생성자 추가
    - Lombok 애너테이션 추가

--- src/main/java/com/sparta/goodbite/domain/menu/entity/Menu.java
+++ src/main/java/com/sparta/goodbite/domain/menu/entity/Menu.java
@@ -3,10 +3,34 @@ package com.sparta.goodbite.domain.menu.entity;
 import com.sparta.goodbite.common.Timestamped;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 public class Menu extends Timestamped {
 
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "restaurant_id", nullable = false)
+//    Restaurant restaurant;
+
     @Id
-    private long id;
+    private Long id;
+
+    private int price;
+    private String name;
+    private String description;
+//    private String imageUrl;
+
+    @Builder
+    public Menu(int price, String name, String description) {
+        this.price = price;
+        this.name = name;
+        this.description = description;
+    }
 }

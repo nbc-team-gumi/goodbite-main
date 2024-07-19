2d29b0bf1b65bab71357d1cbe3bd207687de4610
soyoung <soyoung.yang94@gmail.com>
2024-07-19 14:02:01 +0900

    🎉 Init: 초기설정
    - init commit

---
+++ src/test/java/com/sparta/goodbite/GoodbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.goodbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodbiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

cb465bc82d311740cd3a79d363cabcb200151afe
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-22 20:48:22 +0900

    ✨ feat: Review 생성 요청 DTO 추가
    - 메뉴 ID, 평점, 리뷰 내용을 포함하는 필드 추가
    - 평점 유효성 검사를 위한 RatingConstraint 어노테이션 추가
    - toEntity 메서드를 통해 DTO를 Review 엔티티로 변환 가능하도록 구현

---
+++ src/main/java/com/sparta/goodbite/domain/review/dto/CreateReviewRequestDto.java
@@ -0,0 +1,25 @@
+package com.sparta.goodbite.domain.review.dto;
+
+import com.sparta.goodbite.domain.menu.entity.Menu;
+import com.sparta.goodbite.domain.review.dto.validation.RatingConstraint;
+import com.sparta.goodbite.domain.review.entity.Review;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CreateReviewRequestDto {
+
+    @NotNull(message = "메뉴 ID를 입력해 주세요.")
+    private Long menuId;
+
+    @NotNull(message = "평점을 입력해 주세요.")
+    @RatingConstraint
+    private float rating;
+
+    @NotNull(message = "리뷰 내용을 입력해 주세요.")
+    private String content;
+
+    public Review toEntity(Menu menu) {
+        return Review.builder().rating(rating).content(content).menu(menu).build();
+    }
+}

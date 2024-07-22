eaaf44bb22bb3d8c5fea2c1422ce00e5e2f92ea0
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-22 21:49:44 +0900

    ✨ feat: 리뷰 생성 엔드포인트 추가
    - POST /reviews 엔드포인트에서 CreateReviewRequestDto를 받아 리뷰 생성
    - ReviewService의 createReview 메서드 호출
    - ResponseUtil을 사용해 성공 응답 반환

--- src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
@@ -1,12 +1,29 @@
 package com.sparta.goodbite.domain.review.controller;
 
+import com.sparta.goodbite.common.response.MessageResponseDto;
+import com.sparta.goodbite.common.response.ResponseUtil;
+import com.sparta.goodbite.domain.review.dto.CreateReviewRequestDto;
 import com.sparta.goodbite.domain.review.service.ReviewService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/reviews")
 @RestController
 public class ReviewController {
 
-    private ReviewService reviewService;
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<MessageResponseDto> createReview(
+        @RequestBody @Valid CreateReviewRequestDto createReviewRequestDto) {
+
+        reviewService.createReview(createReviewRequestDto);
+        return ResponseUtil.createOk();
+    }
 }

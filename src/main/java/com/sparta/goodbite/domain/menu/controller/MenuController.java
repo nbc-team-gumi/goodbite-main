d7a110ad6bca95d0858f54109b00a64b2106c322
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-22 15:27:19 +0900

    ✨ feat: 메뉴 수정 엔드포인트 추가
    - PUT /menus/{menuId} 엔드포인트를 추가하여 메뉴 업데이트 기능 구현
    - MenuService의 updateMenu 메서드 사용

--- src/main/java/com/sparta/goodbite/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/goodbite/domain/menu/controller/MenuController.java
@@ -5,6 +5,7 @@ import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
 import com.sparta.goodbite.domain.menu.dto.CreateMenuRequestDto;
 import com.sparta.goodbite.domain.menu.dto.MenuResponseDto;
+import com.sparta.goodbite.domain.menu.dto.UpdateMenuRequestDto;
 import com.sparta.goodbite.domain.menu.service.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,13 @@ public class MenuController {
     @GetMapping("/{menuId}")
     public ResponseEntity<DataResponseDto<MenuResponseDto>> getMenu(@PathVariable Long menuId) {
         return ResponseUtil.findOk(menuService.getMenu(menuId));
+    }
+
+    @PutMapping("/{menuId}")
+    public ResponseEntity<MessageResponseDto> updateMenu(
+        @PathVariable Long menuId, @RequestBody UpdateMenuRequestDto updateMenuRequestDto) {
+
+        menuService.updateMenu(menuId, updateMenuRequestDto);
+        return ResponseUtil.updateOk();
     }
 }

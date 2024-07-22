6f5e969ff7ba9a0a0219bcb4d64514882b428107
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-22 21:08:04 +0900

    ✨ feat: ReviewService 클래스에 createReview 메서드 구현
    - MenuRepository를 통해 메뉴 조회
    - ReviewRepository를 통해 리뷰 저장

--- src/main/java/com/sparta/goodbite/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/goodbite/domain/review/service/ReviewService.java
@@ -1,12 +1,23 @@
 package com.sparta.goodbite.domain.review.service;
 
+import com.sparta.goodbite.domain.menu.entity.Menu;
+import com.sparta.goodbite.domain.menu.repository.MenuRepository;
+import com.sparta.goodbite.domain.review.dto.CreateReviewRequestDto;
 import com.sparta.goodbite.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewService {
 
+    private final MenuRepository menuRepository;
     private final ReviewRepository reviewRepository;
+    
+    @Transactional
+    public void createReview(CreateReviewRequestDto createReviewRequestDto) {
+        Menu menu = menuRepository.findByIdOrThrow(createReviewRequestDto.getMenuId());
+        reviewRepository.save(createReviewRequestDto.toEntity(menu));
+    }
 }

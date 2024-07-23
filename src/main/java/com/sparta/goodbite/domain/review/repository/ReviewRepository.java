8259f9bbcd3cb684a90dc8f58ac0f8e6e63db364
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-23 20:08:27 +0900

    ♻️ refactor: 리뷰 조회 예외 처리 개선
    - findByIdOrThrow 메서드에서 커스텀 예외 ReviewNotFoundException 사용

--- src/main/java/com/sparta/goodbite/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/goodbite/domain/review/repository/ReviewRepository.java
@@ -1,11 +1,14 @@
 package com.sparta.goodbite.domain.review.repository;
 
 import com.sparta.goodbite.domain.review.entity.Review;
+import com.sparta.goodbite.exception.review.ReviewErrorCode;
+import com.sparta.goodbite.exception.review.detail.ReviewNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     default Review findByIdOrThrow(Long reviewId) {
-        return findById(reviewId).orElseThrow(() -> new RuntimeException("임시 메시지"));
+        return findById(reviewId).orElseThrow(
+            () -> new ReviewNotFoundException(ReviewErrorCode.REVIEW_NOT_FOUND));
     }
 }

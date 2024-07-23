ed37011bf161e4f0f14bcb2111df7fef3a9e0be9
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-23 19:39:59 +0900

    ✨ feat: 리뷰 업데이트 엔드포인트 추가
    - 리뷰 업데이트를 위한 PUT 엔드포인트 추가
    - ReviewService의 updateReview 메서드를 호출하여 리뷰 정보 업데이트 기능 구현
    - UpdateReviewRequestDto를 사용하여 리뷰 업데이트 요청을 처리하도록 수정

--- src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
 import com.sparta.goodbite.domain.review.dto.CreateReviewRequestDto;
 import com.sparta.goodbite.domain.review.dto.ReviewResponseDto;
+import com.sparta.goodbite.domain.review.dto.UpdateReviewRequestDto;
 import com.sparta.goodbite.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<MessageResponseDto> createReview(
-        @RequestBody @Valid CreateReviewRequestDto createReviewRequestDto) {
+        @Valid @RequestBody CreateReviewRequestDto createReviewRequestDto) {
 
         reviewService.createReview(createReviewRequestDto);
         return ResponseUtil.createOk();
@@ -35,11 +37,20 @@ public class ReviewController {
     @GetMapping("/{reviewId}")
     public ResponseEntity<DataResponseDto<ReviewResponseDto>> getReview(
         @PathVariable Long reviewId) {
+        
         return ResponseUtil.findOk(reviewService.getReview(reviewId));
     }
 
     @GetMapping
     public ResponseEntity<DataResponseDto<List<ReviewResponseDto>>> getAllReviews() {
         return ResponseUtil.findOk(reviewService.getAllReviews());
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<MessageResponseDto> updateReview(@PathVariable Long reviewId,
+        @Valid @RequestBody UpdateReviewRequestDto updateReviewRequestDto) {
+
+        reviewService.updateReview(reviewId, updateReviewRequestDto);
+        return ResponseUtil.updateOk();
     }
 }

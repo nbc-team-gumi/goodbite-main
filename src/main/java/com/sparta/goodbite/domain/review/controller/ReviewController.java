d80328d7dddb60ff6f8583bd457812e13ee5035f
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-23 11:49:54 +0900

    ✨ feat: 리뷰 단건 조회 엔드포인트 추가
    - ReviewController에 리뷰 조회 기능 추가
    - 리뷰 ID로 리뷰를 조회하고 ReviewResponseDto로 반환

--- src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/goodbite/domain/review/controller/ReviewController.java
@@ -1,12 +1,16 @@
 package com.sparta.goodbite.domain.review.controller;
 
+import com.sparta.goodbite.common.response.DataResponseDto;
 import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
 import com.sparta.goodbite.domain.review.dto.CreateReviewRequestDto;
+import com.sparta.goodbite.domain.review.dto.ReviewResponseDto;
 import com.sparta.goodbite.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,11 @@ public class ReviewController {
 
         reviewService.createReview(createReviewRequestDto);
         return ResponseUtil.createOk();
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<DataResponseDto<ReviewResponseDto>> getReview(
+        @PathVariable Long reviewId) {
+        return ResponseUtil.findOk(reviewService.getReview(reviewId));
     }
 }

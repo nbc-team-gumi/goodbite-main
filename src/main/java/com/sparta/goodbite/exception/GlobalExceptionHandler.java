3c710f0f335430e1bee4bb532bcbba076122da9b
soyoung <soyoung.yang94@gmail.com>
2024-07-24 02:43:37 +0900

    ✨ feat: 리프레시 토큰 예외 처리
    - InvalidRefreshTokenException 클래스 추가
      - AuthErrorCode를 기반으로 한 예외 클래스 구현

--- src/main/java/com/sparta/goodbite/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/goodbite/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sparta.goodbite.exception;
 
 import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
+import com.sparta.goodbite.exception.auth.AuthException;
 import com.sparta.goodbite.exception.menu.MenuException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<MessageResponseDto> handleMenuException(MenuException e) {
         log.error("에러 발생: ", e);
         return ResponseUtil.of(e.getMenuErrorCode().getHttpStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<MessageResponseDto> handleAuthException(AuthException e) {
+        log.error("에러 발생: ", e);
+        return ResponseUtil.of(e.getAuthErrorCode().getHttpStatus(), e.getMessage());
     }
 }

f79e146c2e65c527dce5cee9142840f9663417fb
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-23 20:14:14 +0900

    ✨ feat: GlobalExceptionHandler에 ReviewException 처리 로직 추가
    - ReviewException을 처리하는 예외 핸들러 메서드 추가
    - ReviewException 발생 시 로그를 기록하고, 적절한 응답 메시지 반환

--- src/main/java/com/sparta/goodbite/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/goodbite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.goodbite.exception;
 import com.sparta.goodbite.common.response.MessageResponseDto;
 import com.sparta.goodbite.common.response.ResponseUtil;
 import com.sparta.goodbite.exception.menu.MenuException;
+import com.sparta.goodbite.exception.review.ReviewException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,5 +17,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<MessageResponseDto> handleMenuException(MenuException e) {
         log.error("에러 발생: ", e);
         return ResponseUtil.of(e.getMenuErrorCode().getHttpStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(ReviewException.class)
+    public ResponseEntity<MessageResponseDto> handleReviewException(ReviewException e) {
+        log.error("에러 발생: ", e);
+        return ResponseUtil.of(e.getReviewErrorCode().getHttpStatus(), e.getMessage());
     }
 }

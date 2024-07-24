b7a356ed5de4d77f6d21f96594439ca29f54e90a
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-24 16:54:59 +0900

    ♻️ refactor: 가격 필드를 Integer에서 int로 변경 및 포맷팅 개선

--- src/main/java/com/sparta/goodbite/domain/menu/dto/CreateMenuRequestDto.java
+++ src/main/java/com/sparta/goodbite/domain/menu/dto/CreateMenuRequestDto.java
@@ -9,10 +9,10 @@ import lombok.Getter;
 public class CreateMenuRequestDto {
 
 //    @NotNull(message = "레스토랑 ID를 입력해 주세요.")
-//    private Long restaurant_id;
+//    private Long restaurantId;
 
     @NotNull(message = "메뉴의 가격을 입력해 주세요.")
-    private Integer price;
+    private int price;
 
     @NotBlank(message = "메뉴의 이름을 입력해 주세요.")
     private String name;
@@ -21,6 +21,10 @@ public class CreateMenuRequestDto {
     private String description;
 
     public Menu toEntity() {
-        return Menu.builder().price(price).name(name).description(description).build();
+        return Menu.builder()
+            .price(price)
+            .name(name)
+            .description(description)
+            .build();
     }
 }

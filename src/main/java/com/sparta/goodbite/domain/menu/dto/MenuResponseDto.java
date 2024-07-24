82e9a15041a28e6810914f7cc783bd4d66acdaf6
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-24 16:56:07 +0900

    ♻️ refactor: 포맷팅 개선

--- src/main/java/com/sparta/goodbite/domain/menu/dto/MenuResponseDto.java
+++ src/main/java/com/sparta/goodbite/domain/menu/dto/MenuResponseDto.java
@@ -5,6 +5,9 @@ import com.sparta.goodbite.domain.menu.entity.Menu;
 public record MenuResponseDto(int price, String name, String description) {
 
     public static MenuResponseDto from(Menu menu) {
-        return new MenuResponseDto(menu.getPrice(), menu.getName(), menu.getDescription());
+        return new MenuResponseDto(
+            menu.getPrice(),
+            menu.getName(),
+            menu.getDescription());
     }
 }

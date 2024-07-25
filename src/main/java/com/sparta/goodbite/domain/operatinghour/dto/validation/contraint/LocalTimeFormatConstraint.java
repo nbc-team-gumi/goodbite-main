8f3efddaafd9c5e7651ed16d353c94b576fb86f6
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-25 18:02:18 +0900

    ✨ feat: 시간 포맷 커스텀 유효성 검증 추가

---
+++ src/main/java/com/sparta/goodbite/domain/operatinghour/dto/validation/contraint/LocalTimeFormatConstraint.java
@@ -0,0 +1,21 @@
+package com.sparta.goodbite.domain.operatinghour.dto.validation.contraint;
+
+import com.sparta.goodbite.domain.operatinghour.dto.validation.validator.LocalTimeFormatValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = LocalTimeFormatValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LocalTimeFormatConstraint {
+
+    String message() default "시간은 HH:mm 형식이어야 합니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}

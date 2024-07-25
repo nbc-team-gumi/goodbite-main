454b091faff636acf56d95b3e4e6582051e5a790
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-25 11:35:25 +0900

    ♻️ refactor: 오픈 및 마감 시간 관련 검증 로직 삭제
    - 요청 DTO에서 검증하는 것으로 대체

--- src/main/java/com/sparta/goodbite/domain/operatinghour/service/OperatingHourService.java
+++ src/main/java/com/sparta/goodbite/domain/operatinghour/service/OperatingHourService.java
@@ -7,7 +7,6 @@ import com.sparta.goodbite.domain.operatinghour.repository.OperatingHourReposito
 import com.sparta.goodbite.domain.restaurant.entity.Restaurant;
 import com.sparta.goodbite.domain.restaurant.repository.RestaurantRepository;
 import com.sparta.goodbite.exception.operatinghour.OperatingHourErrorCode;
-import com.sparta.goodbite.exception.operatinghour.detail.OpenTimeLaterThanCloseTimeException;
 import com.sparta.goodbite.exception.operatinghour.detail.OperatingHourDuplicatedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,11 +33,6 @@ public class OperatingHourService {
                 OperatingHourErrorCode.OPERATINGHOUR_DUPLICATED);
         }
 
-        if (createOperatingHourRequestDto.getOpenTime()
-            .isAfter(createOperatingHourRequestDto.getCloseTime())) {
-            throw new OpenTimeLaterThanCloseTimeException(
-                OperatingHourErrorCode.OPERATINGHOUR_OPEN_AFTER_CLOSE);
-        }
         operatingHourRepository.save(createOperatingHourRequestDto.toEntity(restaurant));
     }
 
@@ -47,12 +41,6 @@ public class OperatingHourService {
         UpdateOperatingHourRequestDto updateOperatingHourRequestDto) {
 
         OperatingHour operatingHour = operatingHourRepository.findByIdOrThrow(operatingHourId);
-
-        if (updateOperatingHourRequestDto.getOpenTime()
-            .isAfter(updateOperatingHourRequestDto.getCloseTime())) {
-            throw new OpenTimeLaterThanCloseTimeException(
-                OperatingHourErrorCode.OPERATINGHOUR_OPEN_AFTER_CLOSE);
-        }
         operatingHour.update(updateOperatingHourRequestDto);
     }
 

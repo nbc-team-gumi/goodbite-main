4352eb4ca60b6ed6460fefdca7963f17bb00df9b
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-26 18:19:21 +0900

    ✨ feat: 가격 필드에 @PositiveOrZero 유효성 검증 추가

--- src/main/java/com/sparta/goodbite/domain/menu/dto/UpdateMenuRequestDto.java
+++ src/main/java/com/sparta/goodbite/domain/menu/dto/UpdateMenuRequestDto.java
@@ -1,11 +1,14 @@
 package com.sparta.goodbite.domain.menu.dto;
 
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 @Getter
 public class UpdateMenuRequestDto {
 
+    @PositiveOrZero(message = "가격은 음수가 될 수 없습니다.")
     private Integer price;
+
     private String name;
     private String description;
 }

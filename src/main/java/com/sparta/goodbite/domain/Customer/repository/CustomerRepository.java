521387f426ea75cdf324b1dae1ae21dfbb45d6ee
kim201621123 <66352581+kim201621123@users.noreply.github.com>
2024-07-26 13:48:21 +0900

    🔥 del: 임시 구현 파일 삭제 - customer,owner 디렉토리 및 하위 파일 삭제

--- src/main/java/com/sparta/goodbite/domain/Customer/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.goodbite.domain.Customer.repository;
-
-import com.sparta.goodbite.domain.Customer.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}

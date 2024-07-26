cdded453a977ccd8385256e8870f4c1e1f573894
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-26 19:20:27 +0900

    ✨ feat: Review 생성 시 Customer 정보 포함하도록 DTO 수정
    - CreateReviewRequestDto에 toEntity 메서드에서 Customer 객체를 파라미터로 받아서 Review 엔티티 생성 시 포함
    - Review 엔티티 생성 시 고객 정보가 포함되도록 빌더 패턴 수정

--- src/main/java/com/sparta/goodbite/domain/review/dto/CreateReviewRequestDto.java
+++ src/main/java/com/sparta/goodbite/domain/review/dto/CreateReviewRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.goodbite.domain.review.dto;
 
+import com.sparta.goodbite.domain.customer.entity.Customer;
 import com.sparta.goodbite.domain.menu.entity.Menu;
 import com.sparta.goodbite.domain.review.dto.validation.constraint.RatingConstraint;
 import com.sparta.goodbite.domain.review.entity.Review;
@@ -19,11 +20,12 @@ public class CreateReviewRequestDto {
     @NotNull(message = "리뷰 내용을 입력해 주세요.")
     private String content;
 
-    public Review toEntity(Menu menu) {
+    public Review toEntity(Menu menu, Customer customer) {
         return Review.builder()
             .rating(rating)
             .content(content)
             .menu(menu)
+            .customer(customer)
             .build();
     }
 }

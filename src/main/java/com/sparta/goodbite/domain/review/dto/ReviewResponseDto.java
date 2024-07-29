be083cf18f7df7dc5fefaae605341b2c1014c3be
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-29 14:04:31 +0900

    ♻️ refactor: 리뷰 응답 DTO에 reviewId 포함

--- src/main/java/com/sparta/goodbite/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/com/sparta/goodbite/domain/review/dto/ReviewResponseDto.java
@@ -2,10 +2,11 @@ package com.sparta.goodbite.domain.review.dto;
 
 import com.sparta.goodbite.domain.review.entity.Review;
 
-public record ReviewResponseDto(Long menuId, float rating, String content) {
+public record ReviewResponseDto(Long reviewId, Long menuId, float rating, String content) {
 
     public static ReviewResponseDto from(Review review) {
         return new ReviewResponseDto(
+            review.getId(),
             review.getMenu().getId(),
             review.getRating(),
             review.getContent());

96ae845b8c4fd4285f5895b9431754e4f5e7ee56
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-29 11:18:27 +0900

    ✨ feat: 컨트롤러의 @PreAuthorize 애너테이션 활성화를 위한 @EnableMethodSecurity 추가

--- src/main/java/com/sparta/goodbite/auth/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/goodbite/auth/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity // @PreAuthorize 애너테이션 활성화
 @EnableWebSecurity // Spring Security 사용
 public class WebSecurityConfig {
 

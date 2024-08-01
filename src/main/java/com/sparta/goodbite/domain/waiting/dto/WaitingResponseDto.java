51f903871cc97400ea5947b14512a3d8d192ab31
soyoung <soyoung.yang94@gmail.com>
2024-08-01 16:58:15 +0900

    ✨ feat: 웨이팅 API 응답에 손님 정보 추가

--- src/main/java/com/sparta/goodbite/domain/waiting/dto/WaitingResponseDto.java
+++ src/main/java/com/sparta/goodbite/domain/waiting/dto/WaitingResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.goodbite.domain.waiting.dto;
 
+import com.sparta.goodbite.domain.customer.entity.Customer;
 import com.sparta.goodbite.domain.waiting.entity.Waiting;
 import com.sparta.goodbite.domain.waiting.entity.Waiting.WaitingStatus;
 import java.time.LocalDateTime;
@@ -11,14 +12,25 @@ public record WaitingResponseDto(
     WaitingStatus waitingStatus,
     Long waitingOrder,
     String demand,
+    Long customerId,
+    String customerNickname,
+    Long partySize,
     LocalDateTime createAt,
     LocalDateTime deletedAt
 
 ) {
 
-    public static WaitingResponseDto of(Waiting waiting, String restaurantName) {
-        return new WaitingResponseDto(waiting.getId(), restaurantName, waiting.getStatus(),
-            waiting.getWaitingOrder(), waiting.getDemand(), waiting.getCreatedAt(),
+    public static WaitingResponseDto of(Waiting waiting, String restaurantName, Customer customer) {
+        return new WaitingResponseDto(
+            waiting.getId(),
+            restaurantName,
+            waiting.getStatus(),
+            waiting.getWaitingOrder(),
+            waiting.getDemand(),
+            customer.getId(),
+            customer.getNickname(),
+            waiting.getPartySize(),
+            waiting.getCreatedAt(),
             waiting.getDeletedAt());
     }
 

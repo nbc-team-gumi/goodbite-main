258b9232fac76ad6b12615b19c324d0cba63802c
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-01 16:07:53 +0900

    ♻️ refactor: 불필요한 메서드 삭제

--- src/main/java/com/sparta/goodbite/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/sparta/goodbite/domain/restaurant/service/RestaurantService.java
@@ -1,8 +1,6 @@
 package com.sparta.goodbite.domain.restaurant.service;
 
 import com.sparta.goodbite.common.UserCredentials;
-import com.sparta.goodbite.domain.operatinghour.dto.OperatingHourResponseDto;
-import com.sparta.goodbite.domain.operatinghour.repository.OperatingHourRepository;
 import com.sparta.goodbite.domain.owner.entity.Owner;
 import com.sparta.goodbite.domain.owner.repository.OwnerRepository;
 import com.sparta.goodbite.domain.restaurant.dto.RestaurantRequestDto;
@@ -23,7 +21,6 @@ public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
     private final OwnerRepository ownerRepository;
-    private final OperatingHourRepository operatingHourRepository;
 
     @Transactional
     public void createRestaurant(RestaurantRequestDto restaurantRequestDto, UserCredentials user) {
@@ -43,14 +40,6 @@ public class RestaurantService {
     public List<RestaurantResponseDto> getAllRestaurants() {
         return restaurantRepository.findAll().stream().map(RestaurantResponseDto::from).toList();
     }
-
-    @Transactional(readOnly = true)
-    public List<OperatingHourResponseDto> getAllOperatingHoursByRestaurantId(Long restaurantId) {
-        restaurantRepository.findByIdOrThrow(restaurantId);
-        return operatingHourRepository.findAllByRestaurantId(restaurantId).stream()
-            .map(OperatingHourResponseDto::from).toList();
-    }
-
 
     @Transactional
     public void updateRestaurant(Long restaurantId, RestaurantRequestDto restaurantRequestDto,

bf12d2a7865d64ccc82b69a7856f6efd1589f534
soyoung <soyoung.yang94@gmail.com>
2024-08-04 11:44:27 +0900

    🩹 fix: CORS 설정 수정

--- src/main/java/com/sparta/goodbite/auth/security/CorsConfig.java
+++ src/main/java/com/sparta/goodbite/auth/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sparta.goodbite.auth.security;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,15 +10,22 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    private final List<String> origins = List.of(
+        "http://localhost:3000");
+
     @Bean
     public CorsFilter corsFilter() {
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(origins);
+        config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        config.setExposedHeaders(List.of("Set-Cookie")); // 클라이언트에서 접근 가능한 헤더
+        config.setAllowedMethods(List.of("*"));
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }

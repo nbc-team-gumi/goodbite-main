511417b86ed76a9e248e26b9950bc93e7aec27a1
haeuni00 <leejw6311@naver.com>
2024-08-10 08:37:24 +0900

    ✨ feat: 리뷰 조회시 응답 목록 추가
    - 사용자 닉네임, 가게 이름, 작성일 추가

--- src/main/java/com/sparta/goodbite/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/com/sparta/goodbite/domain/review/dto/ReviewResponseDto.java
@@ -1,14 +1,20 @@
 package com.sparta.goodbite.domain.review.dto;
 
 import com.sparta.goodbite.domain.review.entity.Review;
+import java.time.LocalDateTime;
 
-public record ReviewResponseDto(Long reviewId, Long restaurantId, float rating, String content) {
+public record ReviewResponseDto(Long reviewId, String nickname, Long restaurantId,
+                                String restaurantName, float rating, String content,
+                                LocalDateTime createdAt) {
 
     public static ReviewResponseDto from(Review review) {
         return new ReviewResponseDto(
             review.getId(),
+            review.getCustomer().getNickname(),
             review.getRestaurant().getId(),
+            review.getRestaurant().getName(),
             review.getRating(),
-            review.getContent());
+            review.getContent(),
+            review.getCreatedAt());
     }
 }

7eb367c17ea581e775bb41ba7b4c9e6ba180e5da
soyoung <soyoung.yang94@gmail.com>
2024-08-12 15:45:38 +0900

    ✨ feat: RestTemplate 빈 설정
    - 무한 대기 상태 방지

--- src/main/java/com/sparta/goodbite/common/AppConfig.java
+++ src/main/java/com/sparta/goodbite/common/AppConfig.java
@@ -1,5 +1,7 @@
 package com.sparta.goodbite.common;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +10,12 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+            // RestTemplate 으로 외부 API 호출 시 일정 시간이 지나도 응답이 없을 때
+            // 무한 대기 상태 방지를 위해 강제 종료 설정
+            .setConnectTimeout(Duration.ofSeconds(5)) // 5초
+            .setReadTimeout(Duration.ofSeconds(5)) // 5초
+            .build();
     }
 }

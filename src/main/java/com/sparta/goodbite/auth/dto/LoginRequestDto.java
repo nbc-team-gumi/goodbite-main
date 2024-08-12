bbefc7f799e7ffc998fae367fb2a4219f4a6ff05
soyoung <soyoung.yang94@gmail.com>
2024-08-12 23:04:24 +0900

    ✨ feat: 로그인 요청 isOwner 추가

--- src/main/java/com/sparta/goodbite/auth/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/goodbite/auth/dto/LoginRequestDto.java
@@ -14,6 +14,9 @@ public class LoginRequestDto {
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     private String password;
 
+    @NotBlank(message = "사업자 여부는 필수 입력 항목입니다.")
+    private boolean isOwner;
+
 //    @Pattern(regexp = "^(CUSTOMER|OWNER|ADMIN)$", message = "역할은 CUSTOMER, OWNER, ADMIN 중 하나여야 합니다.")
 //    @NotBlank(message = "역할은 필수 입력 항목입니다.")
 //    private String role;

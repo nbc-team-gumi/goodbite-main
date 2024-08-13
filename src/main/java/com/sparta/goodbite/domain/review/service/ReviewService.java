b40be71b7d4e41ebb365c7e842831078ab40a67e
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-13 22:49:21 +0900

    ♻️ refactor: 리뷰 서비스의 인터페이스화

--- src/main/java/com/sparta/goodbite/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/goodbite/domain/review/service/ReviewService.java
@@ -1,78 +1,22 @@
 package com.sparta.goodbite.domain.review.service;
 
 import com.sparta.goodbite.common.UserCredentials;
-import com.sparta.goodbite.domain.customer.entity.Customer;
-import com.sparta.goodbite.domain.restaurant.entity.Restaurant;
-import com.sparta.goodbite.domain.restaurant.repository.RestaurantRepository;
-import com.sparta.goodbite.domain.review.dto.CreateReviewRequestDto;
 import com.sparta.goodbite.domain.review.dto.ReviewResponseDto;
-import com.sparta.goodbite.domain.review.dto.UpdateReviewRequestDto;
-import com.sparta.goodbite.domain.review.entity.Review;
-import com.sparta.goodbite.domain.review.repository.ReviewRepository;
-import com.sparta.goodbite.exception.auth.AuthErrorCode;
-import com.sparta.goodbite.exception.auth.AuthException;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
-@Service
-public class ReviewService {
+public interface ReviewService<T, U> {
 
-    private final RestaurantRepository restaurantRepository;
-    private final ReviewRepository reviewRepository;
+    void createReview(T dto, UserCredentials user);
 
-    @Transactional
-    public void createReview(CreateReviewRequestDto createReviewRequestDto, UserCredentials user) {
-        Restaurant restaurant = restaurantRepository.findByIdOrThrow(
-            createReviewRequestDto.getRestaurantId());
-        reviewRepository.save(createReviewRequestDto.toEntity(restaurant, (Customer) user));
-    }
+    ReviewResponseDto getReview(Long reviewId);
 
-    @Transactional(readOnly = true)
-    public ReviewResponseDto getReview(Long reviewId) {
-        return ReviewResponseDto.from(reviewRepository.findByIdOrThrow(reviewId));
-    }
+    List<ReviewResponseDto> getAllReviews();
 
-    @Transactional(readOnly = true)
-    public List<ReviewResponseDto> getAllReviews() {
-        return reviewRepository.findAll().stream().map(ReviewResponseDto::from).toList();
-    }
+    List<ReviewResponseDto> getAllReviewsByRestaurantId(Long restaurantId);
 
-    @Transactional(readOnly = true)
-    public List<ReviewResponseDto> getAllReviewsByRestaurantId(Long restaurantId) {
-        return reviewRepository.findAllByRestaurantId(restaurantId).stream()
-            .map(ReviewResponseDto::from).toList();
-    }
+    List<ReviewResponseDto> getMyReviews(UserCredentials user);
 
-    @Transactional(readOnly = true)
-    public List<ReviewResponseDto> getMyReviews(UserCredentials user) {
-        return reviewRepository.findAllByCustomerId(user.getId()).stream()
-            .map(ReviewResponseDto::from).toList();
-    }
+    void updateReview(Long reviewId, U dto, UserCredentials user);
 
-    @Transactional
-    public void updateReview(Long reviewId, UpdateReviewRequestDto updateReviewRequestDto,
-        UserCredentials user) {
-
-        Review review = getReviewByIdAndValidateCustomer(reviewId, user.getId());
-        review.update(updateReviewRequestDto);
-    }
-
-    @Transactional
-    public void deleteReview(Long reviewId, UserCredentials user) {
-        Review review = getReviewByIdAndValidateCustomer(reviewId, user.getId());
-        reviewRepository.delete(review);
-    }
-
-    private Review getReviewByIdAndValidateCustomer(Long reviewId, Long customerId) {
-        Review review = reviewRepository.findByIdOrThrow(reviewId);
-
-        if (!review.getCustomer().getId().equals(customerId)) {
-            throw new AuthException(AuthErrorCode.UNAUTHORIZED);
-        }
-
-        return review;
-    }
+    void deleteReview(Long reviewId, UserCredentials user);
 }

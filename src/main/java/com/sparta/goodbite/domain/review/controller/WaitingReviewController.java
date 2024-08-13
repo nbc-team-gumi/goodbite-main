5a380bcd1ca79097bbb88481a6fac6b802356b4e
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-14 02:57:13 +0900

    🧹 chore: 애너테이션 순서 일관성

--- src/main/java/com/sparta/goodbite/domain/review/controller/WaitingReviewController.java
+++ src/main/java/com/sparta/goodbite/domain/review/controller/WaitingReviewController.java
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/waiting-reviews")
 @RequiredArgsConstructor
+@RequestMapping("/waiting-reviews")
+@RestController
 public class WaitingReviewController {
 
     private final WaitingReviewServiceImpl waitingReviewService;

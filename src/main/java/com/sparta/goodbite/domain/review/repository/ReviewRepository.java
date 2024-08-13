6b99d63f812de7cc32102f4cd3935938b9cfd630
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-13 22:52:46 +0900

    🍺 wip: 임시 주석 처리

--- src/main/java/com/sparta/goodbite/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/goodbite/domain/review/repository/ReviewRepository.java
@@ -1,19 +1,19 @@
-package com.sparta.goodbite.domain.review.repository;
-
-import com.sparta.goodbite.domain.review.entity.Review;
-import com.sparta.goodbite.exception.review.ReviewErrorCode;
-import com.sparta.goodbite.exception.review.detail.ReviewNotFoundException;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-    List<Review> findAllByRestaurantId(Long restaurantId);
-    
-    List<Review> findAllByCustomerId(Long customerId);
-
-    default Review findByIdOrThrow(Long reviewId) {
-        return findById(reviewId).orElseThrow(
-            () -> new ReviewNotFoundException(ReviewErrorCode.REVIEW_NOT_FOUND));
-    }
-}
+//package com.sparta.goodbite.domain.review.repository;
+//
+//import com.sparta.goodbite.domain.review.entity.Review;
+//import com.sparta.goodbite.exception.review.ReviewErrorCode;
+//import com.sparta.goodbite.exception.review.detail.ReviewNotFoundException;
+//import java.util.List;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ReviewRepository extends JpaRepository<Review, Long> {
+//
+//    List<Review> findAllByRestaurantId(Long restaurantId);
+//
+//    List<Review> findAllByCustomerId(Long customerId);
+//
+//    default Review findByIdOrThrow(Long reviewId) {
+//        return findById(reviewId).orElseThrow(
+//            () -> new ReviewNotFoundException(ReviewErrorCode.REVIEW_NOT_FOUND));
+//    }
+//}

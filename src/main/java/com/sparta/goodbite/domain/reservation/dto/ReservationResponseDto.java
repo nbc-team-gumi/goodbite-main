81c113788a85e997427918b9838b0b869295e41d
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-14 19:35:22 +0900

    ✨ feat: 예약 응답 DTO에 메뉴 정보 추가

--- src/main/java/com/sparta/goodbite/domain/reservation/dto/ReservationResponseDto.java
+++ src/main/java/com/sparta/goodbite/domain/reservation/dto/ReservationResponseDto.java
@@ -5,13 +5,18 @@ import com.sparta.goodbite.domain.reservation.entity.ReservationStatus;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 public record ReservationResponseDto(Long reservationId, Long restaurantId, String restaurantName,
                                      Long customerId, LocalDate date, LocalTime time,
                                      String requirement, int partySize, ReservationStatus status,
-                                     LocalDateTime createdAt, LocalDateTime deletedAt) {
+                                     LocalDateTime createdAt, LocalDateTime deletedAt,
+                                     List<MenuItemResponseDto> menuItems) {
 
     public static ReservationResponseDto from(Reservation reservation) {
+        List<MenuItemResponseDto> menuItemResponseDtos = reservation.getReservationMenus().stream()
+            .map(MenuItemResponseDto::from).toList();
+
         return new ReservationResponseDto(
             reservation.getId(),
             reservation.getRestaurant().getId(),
@@ -23,7 +28,8 @@ public record ReservationResponseDto(Long reservationId, Long restaurantId, Stri
             reservation.getPartySize(),
             reservation.getStatus(),
             reservation.getCreatedAt(),
-            reservation.getDeletedAt()
+            reservation.getDeletedAt(),
+            menuItemResponseDtos
         );
     }
 }

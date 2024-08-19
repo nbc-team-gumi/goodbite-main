96827d52ac57ce9c366c301be9c643ed46d520be
kim201621123 <66352581+kim201621123@users.noreply.github.com>
2024-08-19 15:16:38 +0900

    Update PostWaitingRequestDto.java

--- src/main/java/com/sparta/goodbite/domain/waiting/dto/PostWaitingRequestDto.java
+++ src/main/java/com/sparta/goodbite/domain/waiting/dto/PostWaitingRequestDto.java
@@ -17,7 +17,7 @@ public class PostWaitingRequestDto {
     private WaitingStatus waitingStatus;
 
     @Min(value = 1, message = "웨이팅 인원 수는 0명 일 수 없습니다.")
-    @Max(value = 10, message = "웨이팅 인원이 10명 이상 일 시, 가게로 문의주세요.")
+    @Max(value = 10, message = "웨이팅 인원이 11명 이상 일 시, 가게로 문의주세요.")
     @NotNull(message = "웨이팅 인원을 입력해 주세요")
     private Long partySize;
 

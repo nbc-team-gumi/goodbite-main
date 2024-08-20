6f2bef4bb0dd6815a5da33b5e368d8f03350b4b0
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-20 23:04:27 +0900

    ✨ feat: 식당 필터링 조회 QueryDSL로 구현

---
+++ src/main/java/com/sparta/goodbite/domain/restaurant/repository/RestaurantRepositoryCustomImpl.java
@@ -0,0 +1,65 @@
+package com.sparta.goodbite.domain.restaurant.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.goodbite.domain.restaurant.entity.QRestaurant;
+import com.sparta.goodbite.domain.restaurant.entity.Restaurant;
+import com.sparta.goodbite.domain.restaurant.enums.Category;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+@RequiredArgsConstructor
+public class RestaurantRepositoryCustomImpl implements RestaurantRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Page<Restaurant> findPageByFilters(String sido, String sigungu, Category category,
+        Double rating, Pageable pageable) {
+
+        QRestaurant restaurant = QRestaurant.restaurant;
+
+        List<Restaurant> restaurants = queryFactory
+            .selectFrom(restaurant)
+            .where(
+                eqSido(sido),
+                eqSigungu(sigungu),
+                eqCategory(category),
+                goeRating(rating)
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        long total = queryFactory
+            .selectFrom(restaurant)
+            .where(
+                eqSido(sido),
+                eqSigungu(sigungu),
+                eqCategory(category),
+                goeRating(rating)
+            )
+            .fetch()
+            .size();
+
+        return new PageImpl<>(restaurants, pageable, total);
+    }
+
+    private BooleanExpression eqSido(String sido) {
+        return sido != null ? QRestaurant.restaurant.sido.eq(sido) : null;
+    }
+
+    private BooleanExpression eqSigungu(String sigungu) {
+        return sigungu != null ? QRestaurant.restaurant.sigungu.eq(sigungu) : null;
+    }
+
+    private BooleanExpression eqCategory(Category category) {
+        return category != null ? QRestaurant.restaurant.category.eq(category) : null;
+    }
+
+    private BooleanExpression goeRating(Double rating) {
+        return rating != null ? QRestaurant.restaurant.rating.goe(rating) : null;
+    }
+}

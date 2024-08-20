727787c80864298220cc7f4160031331ff8ab50d
sillysillyman <sillysillyman.cs@gmail.com>
2024-08-20 21:24:43 +0900

    ✨ feat: 전체 리뷰에 관한 서비스 구현

---
+++ src/main/java/com/sparta/goodbite/domain/review/service/TotalReviewService.java
@@ -0,0 +1,63 @@
+package com.sparta.goodbite.domain.review.service;
+
+import com.sparta.goodbite.domain.restaurant.entity.Restaurant;
+import com.sparta.goodbite.domain.restaurant.repository.RestaurantRepository;
+import com.sparta.goodbite.domain.review.dto.ReviewResponseDto;
+import com.sparta.goodbite.domain.review.entity.ReservationReview;
+import com.sparta.goodbite.domain.review.entity.WaitingReview;
+import com.sparta.goodbite.domain.review.repository.ReservationReviewRepository;
+import com.sparta.goodbite.domain.review.repository.WaitingReviewRepository;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class TotalReviewService {
+
+    private final ReservationReviewRepository reservationReviewRepository;
+    private final WaitingReviewRepository waitingReviewRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    public Page<ReviewResponseDto> getAllReviewsSortedAndPaged(Pageable pageable,
+        List<ReviewResponseDto> reservationReviews, List<ReviewResponseDto> waitingReviews) {
+
+        List<ReviewResponseDto> allReviews = new ArrayList<>();
+        allReviews.addAll(waitingReviews);
+        allReviews.addAll(reservationReviews);
+
+        allReviews.sort(Comparator.comparing(ReviewResponseDto::createdAt).reversed());
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), allReviews.size());
+        return new PageImpl<>(allReviews.subList(start, end), pageable, allReviews.size());
+    }
+    
+    @Transactional
+    public void updateRestaurantRating(Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.findByIdOrThrow(restaurantId);
+        restaurant.updateRating(calculateAverageRating(restaurant));
+    }
+
+    // Lock 적용 필요
+    private double calculateAverageRating(Restaurant restaurant) {
+        List<ReservationReview> reservationReviews = reservationReviewRepository.findAllByRestaurantId(
+            restaurant.getId());
+        List<WaitingReview> waitingReviews = waitingReviewRepository.findAllByRestaurantId(
+            restaurant.getId());
+
+        double totalRating =
+            reservationReviews.stream().mapToDouble(ReservationReview::getRating).sum() +
+                waitingReviews.stream().mapToDouble(WaitingReview::getRating).sum();
+
+        int totalReviews = reservationReviews.size() + waitingReviews.size();
+
+        return totalReviews > 0 ? totalRating / totalReviews : 0;
+    }
+}

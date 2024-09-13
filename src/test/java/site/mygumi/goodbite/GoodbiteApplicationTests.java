87e5b52eacbc206afd5ce867ff0323dbb13b0512
soyoung <soyoung.yang94@gmail.com>
2024-09-13 14:45:11 +0900

    🩹 fix: 테스트 설정 파일 수정 - 데이터베이스 create-drop 방식 시작 - 포트 8081

--- src/test/java/site/mygumi/goodbite/GoodbiteApplicationTests.java
+++ src/test/java/site/mygumi/goodbite/GoodbiteApplicationTests.java
@@ -3,6 +3,7 @@ package site.mygumi.goodbite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// @ActiveProfiles("test") // application-test.yml
 @SpringBootTest
 class GoodbiteApplicationTests {
 

5cf97d45cf0215ad724b0f4482e31b8a97940a65
soyoung <soyoung.yang94@gmail.com>
2024-10-28 13:01:55 +0900

    📋 docs: 메인클래스(GoodbiteAppliocation) Javadoc 작성

--- src/main/java/site/mygumi/goodbite/GoodbiteApplication.java
+++ src/main/java/site/mygumi/goodbite/GoodbiteApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * <p>
+ * 이 클래스는 {@code main} 메서드를 통해 Spring Boot 애플리케이션을 시작하며, {@code @EnableJpaAuditing} 어노테이션을 통해 JPA의
+ * 자동 감사(Auditing) 기능을 활성화합니다.
+ * </p>
+ *
+ * @author a-white-bit
+ * @version 0.0.1
+ * @since 2024-10-28
+ */
 @SpringBootApplication
 @EnableJpaAuditing
 public class GoodbiteApplication {

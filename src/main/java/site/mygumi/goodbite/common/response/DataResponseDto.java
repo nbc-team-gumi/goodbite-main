494202810d66a541e3e135a3e5161779d70f3525
soyoung <soyoung.yang94@gmail.com>
2024-10-28 13:15:18 +0900

    📋 docs: response util 관련 클래스 Javadoc 추가

--- src/main/java/site/mygumi/goodbite/common/response/DataResponseDto.java
+++ src/main/java/site/mygumi/goodbite/common/response/DataResponseDto.java
@@ -1,5 +1,24 @@
 package site.mygumi.goodbite.common.response;
 
+/**
+ * 상태 코드, 메시지, 응답 데이터를 포함하는 데이터 전송 객체입니다.
+ * <p>
+ * 이 클래스는 HTTP 응답의 상태 코드와 메시지, 그리고 본문 데이터를 하나의 객체로 묶어 반환할 때 사용됩니다. 제네릭 타입 {@code <T>}을 사용하여 다양한 데이터
+ * 타입을 유연하게 담을 수 있습니다.
+ * </p>
+ *
+ * <p>사용 예시:
+ * <pre>
+ * DataResponseDto<String> response = new DataResponseDto<>(200, "성공", "데이터 본문");
+ * </pre>
+ * </p>
+ *
+ * @param <T>        응답 데이터 타입
+ * @param statusCode HTTP 상태 코드
+ * @param message    응답 메시지
+ * @param data       응답 데이터
+ * @author a-white-bit
+ */
 public record DataResponseDto<T>(int statusCode, String message, T data) {
 
 }
